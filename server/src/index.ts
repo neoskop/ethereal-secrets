193fafec0ef89a8b42a2302f9a892768e8386c6a
Arne Diekmann <arne@neoskop.de>
2019-12-18 11:06:06 +0100

    chore: update dependecies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,27 @@
+import { etherealSecrets } from '@neoskop/ethereal-secrets-middleware';
 import * as express from 'express';
-import {etherealSecrets} from '@neoskop/ethereal-secrets-middleware';
 import * as helmet from 'helmet';
 
-let app = express();
-app.use(helmet({
-  noCache: true
-}));
-app.use(helmet.referrerPolicy({policy: 'no-referrer'}));
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"]
-  }
-}));
-app.use('/', etherealSecrets({
-  remote: {
-    enabled: true
-  }
-}));
+const app = express();
+app.use(
+  helmet({
+    noCache: true,
+  }),
+);
+app.use(helmet.referrerPolicy({ policy: 'no-referrer' }));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+    },
+  }),
+);
+app.use(
+  '/',
+  etherealSecrets({
+    remote: {
+      enabled: true,
+    },
+  }),
+);
 app.listen(8080);
