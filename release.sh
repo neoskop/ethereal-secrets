782d589f0514c90a22d1b66a2e45d416c400640b
Arne Diekmann <arne@neoskop.de>
2022-06-30 10:08:22 +0200

    feat: add release script

---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+if [[ "$#" != "1" ]] || [[ ! "$1" =~ ^(patch|minor|major)$ ]]; then
+  echo -e "Usage: $0 \033[1mpatch|minor|major\033[0m"
+  exit 1
+fi
+
+for command in git jq; do
+    if ! command -v $command >/dev/null; then
+        echo -e "Install \033[1m$command\033[0m"
+        exit 1
+    fi
+done
+
+INCREMENT=$1
+
+update_package() {
+    cd $1
+    npm run build &>/dev/null
+    npm version $INCREMENT --no-git-tag-version  &>/dev/null
+    npm publish
+    cd - &>/dev/null
+}
+
+update_package client
+update_package middleware
+update_package server
+version=`cat middleware/package.json | jq -r .version`
+cd server
+npm i @neoskop/ethereal-secrets-middleware@$version
+docker build -t neoskop/ethereal-secrets-server:latest -t neoskop/ethereal-secrets-server:$version .
+docker push neoskop/ethereal-secrets-server:latest
+docker push neoskop/ethereal-secrets-server:$version
+cd - &>/dev/null
+git add .
+git commit -m "chore: Bump version to $version"
+git push
